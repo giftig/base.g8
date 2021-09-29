fed481a2583f621e67c3d14ba76faed538459377
Rob Moore <giftiger.wunsch@xantoria.com>
2021-09-29 14:12:04 +0100

    Add a build script
    Effectively just an alias for the full sbt command for a "full" build,
    since it's a bit of a mouthful.

---
+++ src/main/g8/scripts/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# A "full" build, including coverage and jar assembly.
+# Adjust this according to which stges are relevant for your build
+
+sbt \
+  clean \
+  coverage \
+  test \
+  IntegrationTest/test \
+  coverageAggregate \
+  coverageOff \
+  publish
